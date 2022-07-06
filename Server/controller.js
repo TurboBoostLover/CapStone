a93a5899812044f497b41d43a922f63e69336f28
Nathan Westergard <m800mazdarx8@gmail.com>
2022-07-06 16:14:51 -0600

    Testing

--- Server/controller.js
+++ Server/controller.js
@@ -27,9 +27,49 @@ const homesdata = [
     {
         beds: '5',
         baths: '3',
-        sq_ft: '2700',
+        sq_ft: '2,700',
         aceage: '.25',
         bio: 'This place is cute.',
         price: '450,000'
+    },
+    {
+        beds: '4',
+        baths: '1',
+        sq_ft: '1,750',
+        aceage: '.42' ,
+        bio:'Older but still good.' ,
+        price:'300,000' 
+    },
+    {
+        beds: '4',
+        baths: '2',
+        sq_ft: '2,500',
+        aceage: '.5' ,
+        bio:'Good spec home.' ,
+        price:'375,000' 
+    },
+    {
+        beds: '2',
+        baths: '1',
+        sq_ft: '1500',
+        aceage: '.2' ,
+        bio:'TownHouse.' ,
+        price:'150,000' 
+    },
+    {
+        beds: '8',
+        baths: '8',
+        sq_ft: '10,000',
+        aceage: '12' ,
+        bio:'On a lake.' ,
+        price:'10,000,000' 
+    },
+    {
+        beds: '',
+        baths: '',
+        sq_ft: '',
+        aceage: '' ,
+        bio:'' ,
+        price:'' 
     },
 ]
