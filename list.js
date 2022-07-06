d4cdddb704d227c57052bda4ab45925875f0ea6d
Nathan Westergard <m800mazdarx8@gmail.com>
2022-07-06 15:36:19 -0600

    Testing

--- list.js
+++ list.js
@@ -36,7 +36,7 @@ const seeAll = document.querySelector('#seehomes')
 const populate = () => {
     axios.get("https://capstonef24.herokuapp.com/homesForSale")
     .then((res) => {
-        console.log(res.data)
+        console.log(res.data[0])
         let listings = document.createElement("h2")
         listings.textContent = (res.data)
         document.body.append(listings)
