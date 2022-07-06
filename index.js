851e0298e059b0344b384be708d52ec43cbb09fa
Nathan Westergard <m800mazdarx8@gmail.com>
2022-07-06 09:29:34 -0600

    Adding functionality

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const view = document.querySelector('#view')
 const list = document.querySelector('#list')
 
+
 const see = () => {
     window.location.href=("https://capstonef24.herokuapp.com/buy")
 }
+
+const homes = () => {
+    window.location.href=("https://capstonef24.herokuapp.com/selling")
+}
+
+
+
+
 view.addEventListener('click', see)
